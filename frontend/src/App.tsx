5e47c55f5bf3598635601af3506fbc07bcdbc990
Gabriel Brum da Luz <biel.brumluz@gmail.com>
2024-09-11 20:59:15 -0300

    feat: use tanstack react query to fetch routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,26 @@ import { Dialog } from "./components/ui/dialog";
 import { CreateGoal } from "./components/create-goal";
 import { EmptyGoals } from "./components/empty-goals";
 import { Summary } from "./components/summary";
+import { useQuery } from "@tanstack/react-query";
+import { getSummary } from "./http/get-summary";
 
 export function App() {
+  const { data: summary } = useQuery({
+    queryKey: ["summary"],
+    queryFn: getSummary,
+  });
+
+  function renderContent() {
+    if (!summary) return null;
+
+    if (summary.total > 0) return <Summary />;
+
+    return <EmptyGoals />;
+  }
+
   return (
     <Dialog>
-      <Summary />
-      {/* <EmptyGoals /> */}
+      {renderContent()}
       <CreateGoal />
     </Dialog>
   );
